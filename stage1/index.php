ddab9ce30d4b7f33ccfd59c3b7bd0b1459484397
Paul Tonning <paul.tonning@student.vvc.edu>
2019-04-29 09:33:49 -0700

    Update index.php

--- stage1/index.php
+++ stage1/index.php
@@ -1,3 +1,25 @@
 <?php
-print "TaskList";
- ?>
+
+/**
+The Task class stores all the details of a Task
+
+**/
+class Task{
+    private $completed;
+    private $date_started;
+    private $date_completed;
+
+    public function __construct($date_started,$description) {
+      $this->date_started = $date_started;
+      $this->description = $description;
+    }
+
+    public function set_completed($completed) {
+      $this->completed = $completed;
+    }
+
+    public function get_completed() {
+
+    }
+}
+?>
